5913fa7b9fd89ac5535bd5bd7e85f9fcadec0847
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-10-07 21:56:10 +0530

    feat:fix

--- vite-project/src/Components/RightSection.jsx
+++ vite-project/src/Components/RightSection.jsx
@@ -5,8 +5,8 @@ export default function RightSection() {
   return (
     <div className='w-[35rem] h-[20rem]  flex flex-col justify-center md:items-left items-center gap-5'>
       <div><CommentCard text="Reviews"/></div>
-      <div className='md:pl-24'><CommentCard text="Report Guru"/></div>
-      <div className='md:pl-44'><CommentCard text="BestTech"/></div>
+      <div className='lg:pl-24'><CommentCard text="Report Guru"/></div>
+      <div className='lg:pl-44'><CommentCard text="BestTech"/></div>
     </div>
   )
 }
